70843f30cba72d72187934e9981cd6882b5cbac3
ASHWINRAJ1407 <ashwinraj1407@gmail.com>
2025-10-24 22:31:13 +0530

    commit

---
+++ assignments/src/com/ashwin/assignmentthree/Bank.java
@@ -0,0 +1,13 @@
+package com.ashwin.assignmentthree;
+
+public class Bank {
+    private static int totalAccounts = 0;
+
+    public static void incrementAccountCount() {
+        totalAccounts++;
+    }
+
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+}
